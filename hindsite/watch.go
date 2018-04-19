81b7d091dfc3188e08397ceb0db4eedb1208d2c5
Stuart Rackham <srackham@gmail.com>
2018-04-19 17:26:02 +1200

    watch command: Implemented debounce filter.

--- hindsite/watch.go
+++ hindsite/watch.go
@@ -9,15 +9,55 @@ import (
 	"github.com/fsnotify/fsnotify"
 )
 
+// debounce filters and debounces fsnotify events.
+// If there have been no file system events on the in input channel for the time
+// interval then forward the most recent accepted file system notification event
+// to the output channel.
+func (proj *project) debounce(interval time.Duration, in chan fsnotify.Event, out chan fsnotify.Event) {
+	skip := func(evt fsnotify.Event) bool {
+		result := false
+		var msg string
+		switch {
+		case proj.exclude(evt.Name):
+			msg = "excluded"
+			result = true
+		case evt.Op == fsnotify.Chmod:
+			msg = "ignored"
+			result = true
+		default:
+			msg = "accepted"
+		}
+		proj.verbose("fsnotify: " + msg + ": " + evt.Op.String() + ": " + evt.Name)
+		return result
+	}
+	var nextOut fsnotify.Event
+	timer := time.NewTimer(interval)
+	timer.Stop()
+	for {
+		select {
+		case evt := <-in:
+			if !skip(evt) {
+				nextOut = evt
+				timer.Reset(interval)
+			}
+		case <-timer.C:
+			out <- nextOut
+		}
+	}
+}
+
 func (proj *project) watch() error {
+	// Full rebuild to initialize document and index structures.
 	if err := proj.build(); err != nil {
 		return err
 	}
+	// Create file system watcher.
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		return err
 	}
 	defer watcher.Close()
+	// Watch content and template directories.
 	watcherAddDir := func(dir string) error {
 		proj.verbose("watching: " + dir)
 		return filepath.Walk(dir, func(f string, info os.FileInfo, err error) error {
@@ -36,51 +76,34 @@ func (proj *project) watch() error {
 	if err := watcherAddDir(proj.templateDir); err != nil {
 		return err
 	}
+	// Error channel to exit serve command.
 	done := make(chan error)
+	// Start thread to monitor file system notifications and rebuild website.
 	go func() {
-		mu := sync.Mutex{}
-		prevEvent := fsnotify.Event{}
-		var prevTime time.Time
-		isValid := func(evt fsnotify.Event) bool {
-			result := true
-			concurrent := time.Now().Sub(prevTime) < time.Millisecond*100
-			switch {
-			case proj.exclude(evt.Name):
-				proj.println(0, "EXCLUDED\n")
-				result = false
-			case evt.Op == fsnotify.Chmod:
-				proj.println(0, "IGNORED\n")
-				result = false
-			case evt == prevEvent && concurrent:
-				proj.println(0, "CONCURRENT\n")
-				result = false
-			}
-			prevEvent = evt
-			prevTime = time.Now()
-			return result
-		}
+		out := make(chan fsnotify.Event)
+		go proj.debounce(100*time.Millisecond, watcher.Events, out)
 		for {
+			mu := sync.Mutex{}
 			select {
-			case evt := <-watcher.Events:
+			case evt := <-out:
 				mu.Lock()
 				f := evt.Name
 				proj.println(0, evt.Op.String()+": "+f)
-				if isValid(evt) {
-					proj.println(0, "START BUILD")
-					if err := proj.build(); err != nil {
-						done <- err
-					}
-					proj.println(0, "END BUILD")
-					proj.println(0, "")
+				if err := proj.build(); err != nil {
+					done <- err
 				}
+				proj.println(0, "")
 				mu.Unlock()
 			case err := <-watcher.Errors:
 				done <- err
 			}
 		}
 	}()
+	// Start web server thread.
 	go func() {
+		// TODO: rename to httpserver().
 		done <- proj.serve()
 	}()
+	// Wait for error exit.
 	return <-done
 }
