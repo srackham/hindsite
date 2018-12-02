bc8b123225c24f0621e25e63875e5eda4a3525ba
Stuart Rackham <srackham@gmail.com>
2018-12-03 11:53:00 +1300

    test: server test: Drop redundant code.

--- server_test.go
+++ server_test.go
@@ -10,99 +10,97 @@ import (
 	"time"
 )
 
-func Test_serve(t *testing.T) {
-	t.Run("serve", func(t *testing.T) {
-		tmpdir := path.Join(os.TempDir(), "hindsite-tests")
-		// Initialize temporary directory with test blog.
-		os.RemoveAll(tmpdir)
-		mkMissingDir(tmpdir)
-		proj := newProject()
-		cmd := "hindsite init " + tmpdir + " -template ./testdata/blog/template"
-		args := strings.Split(cmd, " ")
-		code := proj.executeArgs(args)
-		if code != 0 {
-			t.Fatalf("%s", cmd)
-		}
-		if dirCount(path.Join(tmpdir, "template")) != 9 {
-			t.Fatalf("%s: unexpected number of riles in template directory", cmd)
-		}
-		// Start server.
-		proj = newProject()
-		proj.out = make(chan string, 100)
-		proj.in = make(chan string, 1)
-		cmd = "hindsite serve " + tmpdir
-		args = strings.Split(cmd, " ")
-		if err := proj.parseArgs(args); err != nil {
+func Test_server(t *testing.T) {
+	tmpdir := path.Join(os.TempDir(), "hindsite-tests")
+	// Initialize temporary directory with test blog.
+	os.RemoveAll(tmpdir)
+	mkMissingDir(tmpdir)
+	proj := newProject()
+	cmd := "hindsite init " + tmpdir + " -template ./testdata/blog/template"
+	args := strings.Split(cmd, " ")
+	code := proj.executeArgs(args)
+	if code != 0 {
+		t.Fatalf("%s", cmd)
+	}
+	if dirCount(path.Join(tmpdir, "template")) != 9 {
+		t.Fatalf("%s: unexpected number of riles in template directory", cmd)
+	}
+	// Start server.
+	proj = newProject()
+	proj.out = make(chan string, 100)
+	proj.in = make(chan string, 1)
+	cmd = "hindsite serve " + tmpdir
+	args = strings.Split(cmd, " ")
+	if err := proj.parseArgs(args); err != nil {
+		t.Fatalf("serve error: %v", err.Error())
+	}
+	svr := newServer(&proj)
+	go func() {
+		if err := svr.serve(); err != nil {
 			t.Fatalf("serve error: %v", err.Error())
 		}
-		svr := newServer(&proj)
-		go func() {
-			if err := svr.serve(); err != nil {
-				t.Fatalf("serve error: %v", err.Error())
-			}
-		}()
-		waitFor := func(output string) {
-			for {
-				select {
-				case line := <-svr.out:
-					line = strings.Replace(line, "\\", "/", -1) // Normalize MS Windows path separators.
-					if strings.Contains(line, output) {
-						return
-					}
-				case <-time.After(500 * time.Millisecond):
-					t.Fatalf("%s: timed out waiting for: %v", cmd, output)
+	}()
+	waitFor := func(output string) {
+		for {
+			select {
+			case line := <-svr.out:
+				line = strings.Replace(line, "\\", "/", -1) // Normalize MS Windows path separators.
+				if strings.Contains(line, output) {
 					return
 				}
+			case <-time.After(500 * time.Millisecond):
+				t.Fatalf("%s: timed out waiting for: %v", cmd, output)
+				return
 			}
 		}
-		updateAndWait := func(f, text, output string) {
-			err := writeFile(f, text)
-			if err != nil {
-				t.Fatal(err)
-			}
-			waitFor(output)
-		}
-		removeAndWait := func(f, output string) {
-			err := os.Remove(f)
-			if err != nil {
-				t.Fatal(err)
-			}
-			waitFor(output)
-		}
-		waitFor("Press Ctrl+C to stop")
-		// Create new post with copy of existing post.
-		existingfile := path.Join(tmpdir, "content", "posts", "2016-10-18-sed-sed.md")
-		text, err := readFile(existingfile)
+	}
+	updateAndWait := func(f, text, output string) {
+		err := writeFile(f, text)
 		if err != nil {
 			t.Fatal(err)
 		}
-		newfile := path.Join(tmpdir, "content", "posts", "newfile.md")
-		updateAndWait(newfile, text, "error: content/posts/newfile.md: duplicate document build path in: content/posts/2016-10-18-sed-sed.md")
-		// Fix post error.
-		text = strings.Replace(text, "slug: sed-sed", "slug: newfile", 1)
-		updateAndWait(newfile, text, "updated: content/posts/newfile.md")
-		// Change post title.
-		text = strings.Replace(text, "title: Sed Sed", "title: New File", 1)
-		updateAndWait(newfile, text, "updated: content/posts/newfile.md")
-		// Add post tag.
-		text = strings.Replace(text, "tags: [integer,est,sed,tincidunt]", "tags: [integer,est,sed,tincidunt,newfile]", 1)
-		updateAndWait(newfile, text, "updated: content/posts/newfile.md")
-		// Remove post.
-		removeAndWait(existingfile, "removed: content/posts/2016-10-18-sed-sed.md")
-		// Rebuild.
-		svr.in <- "R\n"
-		waitFor("rebuilding...")
-		waitFor("documents: 11")
-		// New static file.
-		newfile = path.Join(tmpdir, "content", "newfile.txt")
-		text = "Hello World!"
-		updateAndWait(newfile, text, "updated: content/newfile.txt")
-		// Remove static file.
-		removeAndWait(newfile, "removed: content/newfile.txt")
-		// Stop serve command.
-		svr.close(nil)
-		time.Sleep(50 * time.Millisecond) // Allow time for serve goroutines to execute cleanup code.
-	})
+		waitFor(output)
+	}
+	removeAndWait := func(f, output string) {
+		err := os.Remove(f)
+		if err != nil {
+			t.Fatal(err)
+		}
+		waitFor(output)
+	}
+	waitFor("Press Ctrl+C to stop")
+	// Create new post with copy of existing post.
+	existingfile := path.Join(tmpdir, "content", "posts", "2016-10-18-sed-sed.md")
+	text, err := readFile(existingfile)
+	if err != nil {
+		t.Fatal(err)
+	}
+	newfile := path.Join(tmpdir, "content", "posts", "newfile.md")
+	updateAndWait(newfile, text, "error: content/posts/newfile.md: duplicate document build path in: content/posts/2016-10-18-sed-sed.md")
+	// Fix post error.
+	text = strings.Replace(text, "slug: sed-sed", "slug: newfile", 1)
+	updateAndWait(newfile, text, "updated: content/posts/newfile.md")
+	// Change post title.
+	text = strings.Replace(text, "title: Sed Sed", "title: New File", 1)
+	updateAndWait(newfile, text, "updated: content/posts/newfile.md")
+	// Add post tag.
+	text = strings.Replace(text, "tags: [integer,est,sed,tincidunt]", "tags: [integer,est,sed,tincidunt,newfile]", 1)
+	updateAndWait(newfile, text, "updated: content/posts/newfile.md")
+	// Remove post.
+	removeAndWait(existingfile, "removed: content/posts/2016-10-18-sed-sed.md")
+	// Rebuild.
+	svr.in <- "R\n"
+	waitFor("rebuilding...")
+	waitFor("documents: 11")
+	// New static file.
+	newfile = path.Join(tmpdir, "content", "newfile.txt")
+	text = "Hello World!"
+	updateAndWait(newfile, text, "updated: content/newfile.txt")
+	// Remove static file.
+	removeAndWait(newfile, "removed: content/newfile.txt")
+	// Stop serve command.
+	svr.close(nil)
+	time.Sleep(50 * time.Millisecond) // Allow time for serve goroutines to execute cleanup code.
 }
 
 // Based onhttps://blog.questionable.services/article/testing-http-handlers-go/
