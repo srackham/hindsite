349ee264656071467e3d98d84368f98e8ceb67a0
Stuart Rackham <srackham@gmail.com>
2018-12-03 13:05:12 +1300

    tests: Added tests for `new` command and refactor `Test_execute()`.

--- exec_test.go
+++ exec_test.go
@@ -5,72 +5,84 @@ import (
 	"path/filepath"
 	"strings"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_execute(t *testing.T) {
-	type test struct {
-		name string
-		proj project
-		cmd  string
-		code int
-		out  string
-	}
 	tmpdir := filepath.Join(os.TempDir(), "hindsite-tests")
-	tests := []test{
-		{
-			"help command",
-			newProject(),
-			"hindsite",
-			0,
-			"Hindsite is a static website generator",
-		},
-		{
-			"build missing",
-			newProject(),
-			"hindsite build missing",
-			1,
-			"error: missing project directory",
-		},
-		{
-			"init builtin blog",
-			newProject(),
-			"hindsite init " + tmpdir + " -builtin blog -v",
-			0,
-			"installing builtin template: blog",
-		},
-		{
-			"init testdata blog",
-			newProject(),
-			"hindsite init " + tmpdir + " -template ./testdata/blog/template",
-			0,
-			"",
-		},
-		{
-			"build blog",
-			newProject(),
-			"hindsite build " + tmpdir + " -content ./testdata/blog/template/init -template ./testdata/blog/template",
-			0,
-			"documents: 11\ndrafts: 1\nstatic: 7",
-		},
+	assert := assert.New(t)
+	exec := func(cmd string) (out string, code int) {
+		proj := newProject()
+		proj.out = make(chan string, 100)
+		args := strings.Split(cmd, " ")
+		code = proj.executeArgs(args)
+		close(proj.out)
+		for line := range proj.out {
+			out += line + "\n"
+		}
+		return out, code
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			os.RemoveAll(tmpdir)
-			mkMissingDir(tmpdir)
-			tt.proj.out = make(chan string, 100)
-			args := strings.Split(tt.cmd, " ")
-			code := tt.proj.executeArgs(args)
-			close(tt.proj.out)
-			var out string
-			for line := range tt.proj.out {
-				out += line + "\n"
-			}
-			if code != tt.code {
-				t.Fatalf("%s: exit code: got %v, want %v", tt.name, code, tt.code)
-			}
-			if !strings.Contains(out, tt.out) {
-				t.Fatalf("%s: output does not contain: %v", tt.name, tt.out)
-			}
-		})
-	}
+
+	out, code := exec("hindsite")
+	assert.Equal(0, code)
+	assert.Contains(out, "Hindsite is a static website generator")
+
+	out, code = exec("hindsite build missing")
+	assert.Equal(1, code)
+	assert.Contains(out, "error: missing project directory")
+
+	os.RemoveAll(tmpdir)
+	mkMissingDir(tmpdir)
+	out, code = exec("hindsite init " + tmpdir + " -builtin blog -v")
+	assert.Equal(0, code)
+	assert.Contains(out, "installing builtin template: blog")
+
+	out, code = exec("hindsite build " + tmpdir)
+	assert.Equal(0, code)
+	assert.Contains(out, "documents: 12\ndrafts: 0\nstatic: 6")
+
+	os.RemoveAll(tmpdir)
+	mkMissingDir(tmpdir)
+	out, code = exec("hindsite init " + tmpdir + " -template ./testdata/blog/template")
+	assert.Equal(0, code)
+	assert.Contains(out, "")
+
+	out, code = exec("hindsite build " + tmpdir)
+	assert.Equal(0, code)
+	assert.Contains(out, "documents: 11\ndrafts: 1\nstatic: 7")
+
+	f := filepath.Join(tmpdir, "content", "new-test-file.md")
+	out, code = exec("hindsite new " + tmpdir + " " + f)
+	assert.Equal(0, code)
+	assert.Contains(out, "")
+	assert.True(fileExists(f))
+	text, _ := readFile(f)
+	assert.Contains(text, "title: New Test File")
+	assert.Contains(text, "date:  "+time.Now().Format("2006-01-02"))
+	assert.Contains(text, "Document content goes here.")
+
+	out, code = exec("hindsite build " + tmpdir)
+	assert.Equal(0, code)
+	assert.Contains(out, "documents: 12\ndrafts: 2\nstatic: 7")
+
+	f = filepath.Join(tmpdir, "content", "posts", "2018-12-01-new-test-file-two.md")
+	out, code = exec("hindsite new " + tmpdir + " " + f)
+	assert.Equal(0, code)
+	assert.Contains(out, "")
+	assert.True(fileExists(f))
+	text, _ = readFile(f)
+	assert.Contains(text, "title: New Test File Two")
+	assert.Contains(text, "date:  2018-12-01")
+	assert.Contains(text, "Test new template.")
+
+	out, code = exec("hindsite new " + tmpdir + " " + f)
+	assert.Equal(1, code)
+	assert.Contains(out, "error: document already exists: content/posts/2018-12-01-new-test-file-two.md")
+
+	out, code = exec("hindsite build " + tmpdir + " -drafts")
+	assert.Equal(0, code)
+	assert.Contains(out, "documents: 13\ndrafts: 0\nstatic: 7")
+
 }
---
+++ testdata/blog/template/posts/new.md
@@ -0,0 +1,7 @@
+---
+title: {{.title}}
+date:  {{.date}}
+draft: true
+---
+
+Test new template.
